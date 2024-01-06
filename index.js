600596b70a1ffc04fe6da1ea47e2359b6c314028
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-01-06 19:41:48 +0530

    authentication done

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const PORT = process.env.PORT||7700;
 require("dotenv").config();
 
 const {connection} = require("./db")
+const {userRouter} = require("./Router/userRouter")
 
 const app = express();
 
  app.use(express.json())
  app.use(cors());
 
-
+app.use("/users",userRouter);
+app.get("/",async(req,res)=>{
+    res.send("Welcome to NotesApp")
+})
 
  app.listen(PORT,async()=>{
     try {
